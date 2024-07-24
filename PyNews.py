467747fa4dad5813226314ada5a7a8304b3eff3e
zFa3 <153332084+zFa3@users.noreply.github.com>
2024-07-23 21:07:38 -0400

    Added prev command, added more user validation

--- PyNews.py
+++ PyNews.py
@@ -15,15 +15,18 @@ def settings():
     print_banner()
     # Filter for .com sites is automatically set to true
     filter_for_dotcom = True
-    ###### Input all the parameters ######
-    topic = input("\033[90mEnter topic: ")
+    topic = ""
+    while not topic:
+        topic = input("\033[90mEnter topic: ")
     since_date = (input("\033[90mPublished since (YYYY-MM-DD) (leave empty for last month): "))
-    articles_per_page = int(input("\033[90mNumber of articles per page? (Max 25): "))
+    try: articles_per_page = int(input("\033[90mNumber of articles per page? (Max 25): "))
+    except: articles_per_page = 20
     max_articles = 25 # the maximum number of articles per page
     articles_per_page = max(min(articles_per_page, max_articles), 5)
     link = f"https://newsapi.org/v2/everything?q={topic}&from={since_date}&sortBy=publishedAt&apiKey={API_KEY}"
     # try to request data from https://newsapi.org, if it fails we catch the error and inform the user
-    try: response = (requests.get(link).json())["articles"]
+    request = (requests.get(link).json())
+    try: response = request["articles"]
     except:
         try:
             if requests.get(link).json()["message"][54] == "You are trying to request results too far in the past.":
@@ -36,7 +39,7 @@ def settings():
             # internet issues, in this case its more useful to
             # return the whole error
             print("---Likely invalid parameters!---\n Error code:")
-            print(requests.get(link).json())
+            print(request)
 
 def settings_advanced():
     countries = ["ae", "ar", "at", "au", "be", "bg", "br", "ca", "ch", "cn", "co", "cu", "cz", "de", "eg", "fr", "gb", "gr", "hk", "hu", "id", "ie", "il", "in", "it", "jp", "kr", "lt", "lv", "ma", "mx", "my", "ng", "nl", "no", "nz", "ph", "pl", "pt", "ro", "rs", "ru", "sa", "se", "sg", "si", "sk", "th", "tr", "tw", "ua", "us", "ve", "za"]
@@ -44,7 +47,6 @@ def settings_advanced():
     global topic, since_date, articles_per_page, max_articles, response, filter_for_dotcom
     print_banner()
     print("\033[92mADVANCED SETTINGS")
-    print("\033[90mLeaving things blank is acceptable")
     # leaving things blank will just default to the website's defualt,
     # rather than inputting them ourselves
     while True:
@@ -57,18 +59,21 @@ def settings_advanced():
                 print(i)
         # if and only if the code is valid, we continue
         elif Country in countries: break
-    topic = input("\033[90mEnter topic: ")
+    topic = ""
+    while not topic:
+        topic = input("\033[90mEnter topic: ")
     # if the input is "top" then we return the top headlines
     if topic.upper() == "TOP":
         link = f"https://newsapi.org/v2/top-headlines?country={Country}&apiKey={API_KEY}"
-        try: response = (requests.get(link).json())["articles"]
+        request = (requests.get(link).json())
+        try: response = request["articles"]
         except:
             try:
                 if requests.get(link).json()["message"][54] == "You are trying to request results too far in the past.":
                     print("---Date is too old!---")
             except:
                 print("---Likely invalid parameters!---\n Error code:")
-                print(requests.get(link).json())
+                print(request)
     else:
         while True:
             Category = input("\033[90mCategory (Help for all categories): ")
@@ -89,14 +94,15 @@ def settings_advanced():
         domains = input("\033[90mDomain to search (url - may return error!)") # say we wanted to only search wsj.com (wall street journal), we can do that
         sort_by = input("\033[90mSort results by:\npublishedAt - Publish date\npopularity - Popularity\n")
         link = f"https://newsapi.org/v2/everything?q={topic}&from={since_date}&category={Category}&country={Country}&domains={domains}&sortBy={sort_by}&pageSize={pageSize}&apiKey={API_KEY}"
-        try: response = (requests.get(link).json())["articles"]
+        request = (requests.get(link).json())
+        try: response = request["articles"]
         except:
             try:
                 if requests.get(link).json()["message"][54] == "You are trying to request results too far in the past.":
                     print("---Date is too old!---")
             except:
                 print("---Likely invalid parameters!---\n Error code:")
-                print(requests.get(link).json())
+                print(request)
 
 def print_banner():
     # since we print the banner multiple times, I made a function that can be called
@@ -106,8 +112,8 @@ def print_banner():
     print("_"*var + "\n\\" + " "*(var - 1) + "/\n/" + "_"*(var - 1) + "\\\n", end = "")
     print("\n")
 
-def printer():
-    # Print the articles and their urls
+def printer(): 
+    print() # Print the articles and their urls
     try:
         for i in response[page_num * articles_per_page:(page_num + 1) * articles_per_page]:
             # filter for .com only websites, can be turned off in advanced settings ("Advanced")
@@ -124,8 +130,8 @@ def printer():
 
 def help():
     print_banner()
-    print("\033[92mPYNEWS - Project made by zFa3 (github.com/zFa3)\n\n\033[90mA Python news app that accesses newsapi.org\nwhich has access to articles published\nby over 150,000 different sources in the last 5 years\nfor current news about various topics\nCommands (non case-sensitive) include\n\n\033[92mNext\033[90m - Next page of results\n\033[92mSettings\033[90m - Opens settings, where you change result parameters\n\033[92mHelp\033[90m - where you are currently at\n\033[92mAdvanced\033[90m - for more advanced settings and fine tuning search results\n\033[92mCredits\033[90m - made possible thanks to newsapi.org!")
-    print()
+    print("\033[92mPYNEWS - Project made by zFa3 (github.com/zFa3)\n\n\033[90mA Python news app that accesses newsapi.org\nwhich has access to articles published\nby over 150,000 different sources in the last 5 years\nfor current news about various topics\nCommands (non case-sensitive) include\n\n\033[92mNext\033[90m - Next page of results\n\033[92mPrevious/Prev\033[90m - Previous page of results\n\033[92mSettings\033[90m - Opens settings, where you change result parameters\n\033[92mHelp\033[90m - where you are currently at\n\033[92mAdvanced\033[90m - for more advanced settings and fine tuning search results\n\033[92mCredits\033[90m - made possible thanks to newsapi.org!\n")
+    print("\033[90mLeaving things blank is acceptable")
 
 
 print_banner()
@@ -138,6 +144,12 @@ while True:
     if action.upper() == "NEXT":
         try:
             page_num += 1; page_num %= math.ceil(len(response)/articles_per_page)
+            printer()
+        except: print("---No topic was selected!---")
+    if action.upper() == "PREVIOUS" or action.upper() == "PREV":
+        try:
+            page_num -= 1
+            if page_num < 0: page_num = math.ceil(len(response)/articles_per_page)
             printer()
         except: print("---No topic was selected!---")
     elif action.upper() == "SETTINGS":
